c4723ae83c26fc25915e061551c12e77689d8163
robincxiao <robincxiao@tencent.com>
2023-05-14 19:19:58 +0800

    完成平台角色权限设置

--- src/api/platform.js
+++ src/api/platform.js
@@ -16,3 +16,19 @@ export function updateRole(data) {
 export function removeRole(data) {
   return post('/platform/role/delete', data)
 }
+
+export function fetchRoleAuth(data){
+  return get('/platform/role/read', data)
+}
+
+export function fetchAllMenu(){
+  return get('/platform/access', {type: 'menu'})
+}
+
+export function fetchAllAccess(){
+  return get('/platform/access', {type: 'rule'})
+}
+
+export function setMenuAccess(data){
+  return post('/platform/role/setrules', data)
+}
--- src/views/platform/index.vue
+++ src/views/platform/index.vue
@@ -19,13 +19,13 @@
 
       <el-table-column label="角色key" width="250px" align="center">
         <template slot-scope="{row}">
-          {{row.role_id}}
+          {{ row.role_id }}
         </template>
       </el-table-column>
 
       <el-table-column label="角色名称" width="300px" align="center">
         <template slot-scope="{row}">
-          {{row.name}}
+          {{ row.name }}
         </template>
       </el-table-column>
 
@@ -37,17 +37,22 @@
 
       <el-table-column label="操作" width="300px" align="center">
         <template slot-scope="{row, $index}">
-          <el-button type="primary" size="mini" @click="checkAuth(row)">
-            查看
+          <el-button v-if="row.id === 2 || row.id === 4" type="info" size="mini" @click="checkAuth(row)">
+            查看权限
           </el-button>
+          <div v-else>
+            <el-button type="success" size="mini" @click="updateRole(row)">
+              修改
+            </el-button>
 
-          <el-button type="success" size="mini" @click="updateRole(row)">
-            修改
-          </el-button>
+            <el-button type="primary" size="mini" @click="checkAuth(row)">
+              配置权限
+            </el-button>
 
-          <el-button type="danger" size="mini" @click="delRole(row)">
-            删除
-          </el-button>
+            <el-button type="danger" size="mini" @click="delRole(row)">
+              删除
+            </el-button>
+          </div>
         </template>
       </el-table-column>
     </el-table>
@@ -77,27 +82,35 @@
       </el-form>
       <span style="display:block;text-align: center">
         <el-button @click="createRoleDialogVisible=false">取 消</el-button>
-        <el-button type="primary" @click="submitRole">{{roleDialogType === 'create' ? '创 建' : '更 新'}}</el-button>
+        <el-button type="primary" @click="submitRole">{{ roleDialogType === 'create' ? '创 建' : '更 新' }}</el-button>
       </span>
     </el-dialog>
 
     <el-dialog
-      title="查看权限"
+      :title="showCheckbox ? '配置权限' : '查看权限'"
       :visible.sync="dialogVisible"
       @close='closeCheckAuthDialog'>
-      <el-form :model="roleForm" label-width="130px">
+      <el-form :model="roleForm" label-width="130px" v-loading="dialogLoadingShow">
         <el-form-item label="角色名称">
-          {{roleForm.name}}
+          {{ roleForm.name }}
         </el-form-item>
 
         <el-form-item label="菜单">
-          <el-tree :data="menuData" :props="menuProps"/>
+<!--          因为el-tree设置默认选中项有缓存不会重新加载数据，因此使用v-if的方式让其重新渲染-->
+          <el-tree ref="menuRef" v-if="loadTree" :data="showCheckbox ? all.menus : role.menus" :props="treeProps" :show-checkbox="showCheckbox"
+                   node-key="id" :default-checked-keys="role.menuId"/>
         </el-form-item>
 
         <el-form-item label="权限">
-          <el-tree :data="authData" :props="authProps"/>
+          <el-tree ref="accessRef" v-if="loadTree" :data="showCheckbox ? all.accesses : role.accesses" :props="treeProps" :show-checkbox="showCheckbox"
+                   node-key="id"  :default-checked-keys="role.accessId"/>
         </el-form-item>
       </el-form>
+
+      <span style="display:block;text-align: center">
+        <el-button @click="dialogVisible=false">取 消</el-button>
+        <el-button type="primary" @click="updateMenuAndAccess" v-loading="btnLoading">{{btnLoading ? '提交中...' : '提 交'}}</el-button>
+      </span>
     </el-dialog>
   </div>
 </template>
@@ -105,8 +118,8 @@
 <script>
 import waves from '@/directive/waves' // waves directive
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
-import {fetchRoleAuth} from '@/api/setting'
-import {fetchRoles, createRole, updateRole, removeRole} from '@/api/platform'
+import {fetchRoles, createRole, updateRole, removeRole, fetchRoleAuth, fetchAllMenu, fetchAllAccess
+ ,setMenuAccess} from '@/api/platform'
 import Navbar from "@/layout/components/Navbar";
 
 export default {
@@ -126,13 +139,19 @@ export default {
       roleForm: {
         name: undefined
       },
-      menuData: [],
-      menuProps: {
-        children: 'children',
-        label: 'title'
+      showCheckbox: true,
+      all: {
+        menus: [],
+        accesses: []
       },
-      authData: [],
-      authProps: {
+      role: {
+        id: undefined,
+        menus: [],
+        accesses: [],
+        menuId: [],
+        accessId: [],
+      },
+      treeProps: {
         children: 'children',
         label: 'title'
       },
@@ -153,6 +172,9 @@ export default {
           {required: true, message: '请输入角色唯一标识', trigger: 'blur'},
         ]
       },
+      dialogLoadingShow: false,
+      loadTree: false,
+      btnLoading: false
     }
   },
   created() {
@@ -163,17 +185,39 @@ export default {
       this.loadingShow = true
       const res = await fetchRoles(this.listQuery)
       this.loadingShow = false
-      if (res.code === 20000) {
-        this.list = res.data.items
-        this.total = res.data.total
+      this.list = res.data.items
+      this.total = res.data.total
+    },
+    async getAllMenuAndAccesses(){
+      //加载所有menu和access
+      if(this.all.menus.length === 0){
+        const res = await fetchAllMenu()
+        this.all.menus = res.data.items
+      }
+      if(this.all.accesses.length === 0) {
+        const res = await fetchAllAccess()
+        this.all.accesses = res.data.items
       }
     },
     async checkAuth(row) {
+      this.showCheckbox = !(row.id === 2 || row.id === 4)
+      this.loadTree = false
       this.dialogVisible = true
+
+      //加载所有menu和access
+      this.dialogLoadingShow = true
+      await this.getAllMenuAndAccesses()
+
+      //加载该角色对应的menu和access
       this.roleForm.name = row.name
       const res = await fetchRoleAuth({id: row.id})
-      this.menuData= res.data.menus
-      this.authData= res.data.accesses
+      this.role.id = row.id
+      this.role.menus = res.data.menus
+      this.role.menuId = res.data.menusId
+      this.role.accesses = res.data.accesses
+      this.role.accessId = res.data.accessesId
+      this.loadTree = true//开始让tree渲染新的选中项
+      this.dialogLoadingShow = false
     },
     closeCheckAuthDialog() {
 
@@ -202,22 +246,22 @@ export default {
       })
     },
     async commit() {
-      if(this.roleDialogType === 'update') {
+      if (this.roleDialogType === 'update') {
         await updateRole(this.createRoleForm)
         this.$message.success('更新成功')
-      }else {
+      } else {
         await createRole(this.createRoleForm)
         this.$message.success('创建成功')
       }
       this.createRoleDialogVisible = false
       await this.getList()
     },
-    async updateRole(row){
+    async updateRole(row) {
       this.roleDialogType = 'update'
       this.createRoleForm = row
       this.createRoleDialogVisible = true
     },
-    delRole(row){
+    delRole(row) {
       this.$confirm('此操作将永久删除该角色, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
@@ -227,6 +271,15 @@ export default {
         this.$message.success('删除成功!');
         await this.getList()
       })
+    },
+    async updateMenuAndAccess(){
+      this.btnLoading = true
+      await setMenuAccess({role_id: this.role.id, access_ids: [...this.$refs.menuRef.getCheckedKeys(),
+        ...this.$refs.accessRef.getCheckedKeys()]})
+      this.btnLoading = false
+      this.dialogVisible = false
+      this.$message.success('更新成功');
+      await this.getList()
     }
   }
 }
